2e228db925bae42c1b2557f476e0c98496035e8e
josephMG <joseph780116.mg@gmail.com>
2015-09-10 04:22:15 +0800

    add angular flash.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,5 +16,6 @@
 //= require angular-route/angular-route
 //= require angular-resource/angular-resource
 //= require angular-rails-templates
+//= require angular-flash/dist/angular-flash
 //= require_tree ../templates
 //= require_tree .
